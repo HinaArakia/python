a36ca979e99313bb9863d25fe02811ee2776ea9e
Hina Araki <h.araki.aforce@gmail.com>
2020-07-02 18:11:12 +0900

    refs#01:Python基礎課題4修正

--- Python1/python1_test4.py
+++ Python1/python1_test4.py
@@ -2,11 +2,16 @@ debt = float(250000)
 interest_rate = float(0.14)
 repayment =  float(30000)
 month = 0
-#返済額を下回った時はelseで分岐
-while (debt > repayment):
+
+while debt > 0:
     month += 1
-    debt = debt*(1+ interest_rate/12) - repayment
-    print(str(month)+"ヶ月目: 返済額=",repayment,"円","残り",float(debt),"円")
-else:
-    month += 1
-    print(str(month)+"ヶ月目:返済額",float(debt),",返済完了。") 
+    #　その月の返済額に年利を月割りした分を上乗せする
+    debt = debt*(1 + interest_rate / 12)
+    
+    if debt > repayment:
+        debt -= repayment
+        print("{}か月目:返済額:{}円,残り{}円".format(month,repayment,debt))
+    else:
+        repayment = debt
+        debt = 0
+    print("{}か月目 : 返済額={}円,返済完了".format(month,repayment)) 
