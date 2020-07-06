30986bbd6b2c33bb950980a2b9013c37b0381c75
Hina Araki <h.araki.aforce@gmail.com>
2020-07-06 21:13:04 +0900

    ref #5:2020年7月6日　21時13分　　課題7修正　(アブストラクトメソッド定義,子クラスに継承)

--- Python1/python3_test7.py
+++ Python1/python3_test7.py
@@ -1,31 +1,39 @@
+#Vihicleクラスにmoveメソッドを定義
 class Vihicle:
     def __init__(self, driver, engine, do):
-       self.driver = driver
-       self.engine = engine
-       self.do = do
-    def drive(self): 
+        self.driver = driver
+        self.engine = engine
+        self.do = do
+
+    def move(self): 
         print( self.driver +  "は" + self.engine + "を" + self.do + "と進みます。")
 
+#子クラスで継承
 class Car(Vihicle):
-    pass
+    def move(self):
+     super().move()
 
 class Bike(Vihicle):
-    pass
+    def move(self):
+     super().move()
 
 class Boat(Vihicle):
-    pass
+    def move(self):
+     super().move()
 
 class Airplane(Vihicle):
-    pass
+    def move(self):
+     super().move()
 
 car = Car("自動車","アクセル","踏む")
 bike = Bike("自転車","ペダル","漕ぐ")
 boat = Boat("船","スクリュー","回す")
 airplane = Airplane("飛行機","プロペラ","回す")
 
-car.drive()
-bike.drive()
-boat.drive()
-airplane.drive()
+
+car.move()
+bike.move()
+boat.move()
+airplane.move()
 
 
