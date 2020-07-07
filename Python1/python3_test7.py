43d470a87341e6082b28fb50a8754a4082b9400c
Hina Araki <h.araki.aforce@gmail.com>
2020-07-07 11:14:32 +0900

    refs #5:2020年7月7日11時14分　課題7修正　抽象クラス設定

--- Python1/python3_test7.py
+++ Python1/python3_test7.py
@@ -1,35 +1,37 @@
-#Vihicleクラスにmoveメソッドを定義
-class Vihicle:
+from abc import ABCMeta, abstractclassmethod
+
+#Vihicleクラス(抽象クラス)にmoveメソッドを定義
+class Vihicle(metaclass=ABCMeta):
     def __init__(self, driver, engine, do):
         self.driver = driver
         self.engine = engine
         self.do = do
 
-    def move(self): 
-        print( self.driver +  "は" + self.engine + "を" + self.do + "と進みます。")
+    @abstractclassmethod
+    def move(self):
+      pass
 
 #子クラスで継承
 class Car(Vihicle):
-    def move(self):
-     super().move()
+   def move(self):
+      print( self.driver + "は" + self.engine + "を" + self.do + "と進みます。" )
 
 class Bike(Vihicle):
     def move(self):
-     super().move()
-
+      print( "自動車は" + self.engine + "を" + self.do + "と進みます。" )
+    
 class Boat(Vihicle):
     def move(self):
-     super().move()
-
+      print( self.driver + "は" + self.engine + "を" + self.do + "と進みます。" )
+      
 class Airplane(Vihicle):
     def move(self):
-     super().move()
+      print( self.driver + "は" + self.engine + "を" + self.do + "と進みます。" )
 
 car = Car("自動車","アクセル","踏む")
 bike = Bike("自転車","ペダル","漕ぐ")
 boat = Boat("船","スクリュー","回す")
 airplane = Airplane("飛行機","プロペラ","回す")
-
 
 car.move()
 bike.move()
